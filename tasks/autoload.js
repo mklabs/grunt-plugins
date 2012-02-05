e6897905c991db32b1f3a74c903496dc41406bb6
Mickael Daniel <daniel.mickael@gmail.com>
2012-02-05 14:09:38 +0100

    some cleanup

--- tasks/autoload.js
+++ tasks/autoload.js
@@ -1,7 +1,17 @@
 
-
 var fs = require('fs'),
   path = require('path');
+
+// autoload thing
+//
+// pretty basic for now, but does the trick. This file, being
+// a `.js` file inside the `~/.grunt/tasks` directory, is automatically
+// loaded by grunt.
+
+// The idea is that any other folder than the `init` one is required to
+// load-in any additional task or helper. This autoload mechanism + the
+// npm install script enables task to be contained within their own
+// module, with dependencies properly sandboxed.
 
 fs.readdirSync(__dirname).forEach(function(f) {
   var filepath = path.join(__dirname, f);
