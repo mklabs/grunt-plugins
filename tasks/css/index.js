baebd8c809ca99fe15cd7c365c5247c8330c638d
Mickael Daniel <daniel.mickael@gmail.com>
2012-04-04 21:07:29 +0200

    more cleanup

--- tasks/css/index.js
+++
@@ -1,4 +0,0 @@
-
-var cssmin = require('sqwish');
-
-task.registerTask("fooooobar", "awesome if it works", console.log.bind(console));
--- tasks/css/test.js
+++
@@ -1,9 +0,0 @@
-
-
-exports.foo = {
-  'should be': function(test) {
-    test.expect(1);
-    test.equal('good', 'good');
-    test.done();
-  }
-};
