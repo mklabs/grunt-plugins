af442d05e16b466e7147ac66398a39e9349d473d
Mickael Daniel <daniel.mickael@gmail.com>
2012-04-10 17:10:46 +0200

    update the plugin refs in gruntfile / package.json

--- grunt.js
+++ grunt.js
@@ -1,8 +1,17 @@
 module.exports = function(grunt) {
 
+  //
   // Register each plugin in our grunt setup
+  //
 
+  // grunt help
   grunt.task.loadNpmTasks('grunt-help');
+
+  // grunt impress
+  grunt.task.loadNpmTasks('grunt-impress');
+
+  // grunt fetch
+  grunt.task.loadNpmTasks('grunt-fetch');
 
   // Default task.
   // My default task is the help one.
